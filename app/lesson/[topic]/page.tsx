2660858e7a6d018c048682b4a19eef008bd71307
VennCoder <akshayvennikkal.av@gmail.com>
2026-02-28 18:46:05 +0530

    Fix: cast topic to Topic type in page

--- app/lesson/[topic]/page.tsx
+++ app/lesson/[topic]/page.tsx
@@ -1,5 +1,6 @@
 import { LessonFlow } from "@/components/lesson/LessonFlow";
 import { TOPICS } from "@/data/topics";
+import { Topic } from "@/types";
 import { notFound } from "next/navigation";
 
 interface Props {
@@ -14,5 +15,5 @@ export default async function LessonPage({ params }: Props) {
   const { topic } = await params;
   const valid = TOPICS.find((t) => t.id === topic);
   if (!valid) notFound();
-  return <LessonFlow topic={topic} />;
+  return <LessonFlow topic={topic as Topic} />;
 }
